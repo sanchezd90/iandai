766d7ae2d05eae1c9c8ebcf68ea39903803fce6f
Daniel <daniel.rueda@ibisdev.tech>
2023-12-14 11:51:44 -0300

    exercise model updated

--- src/models/exercise.js
+++ src/models/exercise.js
@@ -4,6 +4,7 @@ const exerciseSchema = new mongoose.Schema({
   name: { type: String, required: true },
   systemPrompt: { type: String, required: true },
   instructions: { type: String, required: true },
+  responseTemplate: { type: String, required: true },
   // other exercise details
 });
 
